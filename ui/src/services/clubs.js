9b1658adbacffb757f051ef0352ccd8cb171b1de
Laurin Novak <laurin@pellervoinen.com>
2022-11-16 03:14:18 +0200

    fix json splitting

--- ui/src/services/clubs.js
+++ ui/src/services/clubs.js
@@ -1,10 +1,40 @@
+import axios from 'axios'
+
 const baseUrl = '/api/clubs'
 
+// the proper handling of concatenated JSON is a topic for another day
 /* eslint-disable no-undef */
 const getAll = async (dataHandler) => {
+    const splitStream = () => {
+        let buffer = ''
+
+        return new TransformStream({
+            transform(chunk, controller) {
+                buffer += chunk
+
+                buffer = buffer.replaceAll('}{', '}\n{')
+                const parts = buffer.split('\n')
+                parts.slice(0, -1).forEach((part) => controller.enqueue(part))
+                buffer = parts[parts.length - 1]
+            },
+            flush(controller) {
+                if (buffer) controller.enqueue(buffer)
+            },
+        })
+    }
+
+    const handleReader = async (reader) => {
+        const { value, done } = await reader.read()
+        if (!done) {
+            dataHandler(value)
+            await handleReader(reader)
+        }
+    }
+
     const response = await fetch(baseUrl)
     const results = response.body
         .pipeThrough(new TextDecoderStream())
+        .pipeThrough(splitStream())
         .pipeThrough(
             new TransformStream({
                 transform(chunk, controller) {
@@ -15,15 +45,12 @@ const getAll = async (dataHandler) => {
 
     const reader = results.getReader()
 
-    const handleReader = async (reader) => {
-        const { value, done } = await reader.read()
-        if (!done) {
-            dataHandler(value)
-            await handleReader(reader)
-        }
-    }
-
     await handleReader(reader)
 }
 
-export default { getAll }
+const create = async (newObject) => {
+    const response = await axios.post(baseUrl, newObject)
+    return response.data
+}
+
+export default { getAll, create }
